7aa068cdd4a651b96c4bed8356067ed7d5d612b6
psarathi <parthasarathi@gmail.com>
2014-03-06 00:17:23 -0800

    YAML to RESX converter

---
+++ Readme.md
@@ -0,0 +1,21 @@
+#YAML to RESX converter
+
+###Overview
+This program can be used to convert YAML to key-value pairs and then, to the RESX XML format.
+However, this tool is generic and can be used to convert YAML to any specified format.
+
+
+###Installation
+1. git clone https://gist.github.com/3293a8725bce8f8a30b0.git
+2. Open the **converter.html** under the YAMLConverter directory
+
+###Usage
+1. Enter the YAML text to be converted in the box labeled as **YAML**
+2. Enter the delimiter to be used between the object and its property. "." is the default e.g. foo.bar
+3. Click on the **YAML -> KeyValue Pair** button and the keyvalue pairs will be displayed under the **KeyValue Pair** box
+4. Enter the format to be used to generate the final result in the text box under the **KeyValue Pair -> Result** button.
+The default format is **&lt;data name={key} xml:space="preserve"&gt; &lt;value&gt;{value}&lt;/value&gt;&lt;/data&gt;**. The {key} and {value}
+will be replaced by the keys and values respectively generated in the previous step.
+5. Click on the **KeyValue Pair -> Result** button and the result will be displayed under the **Conversion Result** box
+6. Click to the **Copy Result** button to copy the result
+7. Click the **Clear All** button to clear all text
---
+++ js/yamlconverter.js
@@ -0,0 +1,175 @@
+(function () {
+    var OSName = "Unknown OS";
+    if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+    if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+    var btnConvert = document.getElementById('jtoh');
+    var btnClear = document.getElementById('clear');
+    var btnFinish = document.getElementById('htor');
+    var btnCopyResult = document.getElementById('copyResult');
+    var txtSource = document.getElementById('source');
+    var txtResult = document.getElementById('result');
+    var txtFinalResult = document.getElementById('finalResult');
+    var txtDelimiter = document.getElementById('delimiter');
+    var txtTemplate = document.getElementById('template');
+    var defaultTemplate = '<data name={key} xml:space="preserve"> <value>{value}</value></data>';
+    var defaultDelimiter = '.';
+
+    // Event Listeners
+    btnConvert.addEventListener('click', function () {
+        var sourceValue = txtSource.value;
+        var sourceLines = sourceValue.split('\n');
+        txtResult.value = converter.convert(sourceLines);
+    });
+
+    btnClear.addEventListener('click', function () {
+        var inputs = document.getElementsByTagName('input');
+        var textareas = document.getElementsByTagName('textarea');
+        for (var i = inputs.length - 1; i >= 0; i--) {
+            inputs[i].value = '';
+        }
+        ;
+        for (var j = textareas.length - 1; j >= 0; j--) {
+            textareas[j].value = '';
+        }
+        ;
+    });
+
+    btnFinish.addEventListener('click', function () {
+        if (!txtResult.value.trim()) {
+            return;
+        }
+
+        txtFinalResult.value = converter.convertToResx(txtResult.value.split('\n'));
+    });
+
+    btnCopyResult.addEventListener('click', function () {
+        var copy = OSName === 'Windows' ? 'Ctrl+C' : 'Cmd+C',
+            copyMessage = "Copy to clipboard: " + copy + ", Enter";
+        window.prompt(copyMessage, txtFinalResult.value);
+    });
+
+    var converter = {
+        convertToResx: function (linesToBeConverted) {
+            var resxResult = [];
+            for (var i = 0; i < linesToBeConverted.length; i++) {
+                if (!linesToBeConverted[i].trim()) {
+                    continue;
+                }
+
+                var keyValuePair = linesToBeConverted[i].split('=');
+                var template = txtTemplate.value.trim() || defaultTemplate;
+                resxResult.push(template.replace(/{key}/g, keyValuePair[0]).replace(/{value}/g, keyValuePair[1]).replace(/"/g, ''));
+            }
+
+            return resxResult.join('\n');
+        },
+        convert: function (yamlLines) {
+            var tagTracker = [];
+            var keyValueResult = [];
+            var prefix = '';
+            var setPrefix = function () {
+                prefix = prefix ? prefix + delimiter + yamlProperty : yamlProperty;
+            };
+            var getKeyValuePair = function () {
+                return prefix + '=' + propertyValue;
+            };
+
+            for (var i = 0; i < yamlLines.length; i++) {
+                var yamlLine = yamlLines[i];
+                if (!yamlLine) continue;
+                var leadingSpaceCount = this.getLeadingSpacesCount(yamlLine);
+                var yamlProperty = this.getYAMLProperty(yamlLine);
+                var propertyValue = this.getPropertyValue(yamlLine);
+                var previousTag = tagTracker.length == 0 ? null : tagTracker[tagTracker.length - 1];
+                propertyValue = yamlLine.replace(yamlProperty + ":", '').trim();
+                prefix = prefix || '';
+                var delimiter = txtDelimiter.value.trim() || defaultDelimiter;
+
+                if (!previousTag) {
+                    tagTracker.push({
+                        'tag': prefix + yamlProperty,
+                        'space': leadingSpaceCount
+                    });
+
+                    setPrefix();
+                    if (!propertyValue) {
+                        continue;
+                    }
+
+                    keyValueResult.push(getKeyValuePair());
+                    continue;
+                }
+
+                if (leadingSpaceCount > previousTag.space) {
+                    // Child
+                    tagTracker.push({
+                        'tag': prefix + yamlProperty,
+                        'space': leadingSpaceCount
+                    });
+
+                    setPrefix();
+                    if (!propertyValue) {
+                        continue;
+                    }
+
+                    keyValueResult.push(getKeyValuePair());
+                    continue;
+
+                }
+                if (leadingSpaceCount == previousTag.space) {
+                    // Sibling
+                    tagTracker.pop();
+                    tagTracker.push({
+                        'tag': prefix + yamlProperty,
+                        'space': leadingSpaceCount
+                    });
+
+                    setPrefix();
+                    if (!propertyValue) {
+                        continue;
+                    }
+
+                    keyValueResult.push(getKeyValuePair());
+                    continue;
+                }
+                if (leadingSpaceCount < previousTag.space) {
+                    // Parent
+                    var tagCountSearch = leadingSpaceCount;
+                    var trackerLength = tagTracker.length;
+                    while (tagCountSearch !== tagTracker[trackerLength - 1].space) {
+                        tagTracker.pop();
+                        trackerLength--;
+                        prefix = prefix.substring(0, prefix.lastIndexOf(delimiter));
+                    }
+                    tagTracker.pop();
+                    tagTracker.push({
+                        'tag': yamlProperty,
+                        'space': leadingSpaceCount
+                    });
+
+                    setPrefix();
+                    if (!propertyValue) {
+                        keyValueResult.push("");
+                        continue;
+                    }
+
+                    keyValueResult.push(getKeyValuePair());
+                }
+            }
+            return keyValueResult.join('\n');
+        },
+        getPropertyValue: function (yamlLine) {
+            return yamlLine.substring(yamlLine.indexOf(':'));
+        },
+        getLeadingSpacesCount: function (yamlLine) {
+            var matches = /^[\s\t\r\n]+/.exec(yamlLine);
+            if (!matches) return 0;
+            return matches[0].length;
+        },
+        getYAMLProperty: function (yamlLine) {
+            var tag = yamlLine.substring(0, yamlLine.indexOf(':'));
+            console.log(tag.trim());
+            return tag ? tag.trim() : '';
+        }
+    };
+})();
